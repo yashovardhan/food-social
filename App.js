7b0c19ee7b44c44793025d625431a851b10a3fc9
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-08-16 04:04:51 +0530

    App.js: Render Router and intergrate redux

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import store from './src/store';
+
+import Router from './src/screens/Router';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <Router />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
