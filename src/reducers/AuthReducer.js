c553afde37f010c0b7a0cba9051ccbc30da21352
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-08-16 21:44:10 +0530

    reducers: Boilerplate setup

---
+++ src/reducers/AuthReducer.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = { code: null, error: null, loading: false };
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
