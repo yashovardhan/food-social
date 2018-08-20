20d25d98c70b24745ca31c1c4bbdd14adb25cd09
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-08-21 03:12:30 +0530

    NotificationScreen: Add Header

--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Font } from 'expo';
+import { Header } from 'react-native-elements';
+
+import styles from '../styles/NotificationScreenStyle';
 
 class NotificationScreen extends Component {
+  state = {
+    fontLoaded: false,
+  };
+
   async componentDidMount() {
     await Font.loadAsync({
       'poppins-bold': require('../../assets/fonts/Poppins-Bold.otf'),
@@ -10,17 +17,23 @@ class NotificationScreen extends Component {
       'poppins-regular': require('../../assets/fonts/Poppins-Regular.otf'),
       'poppins-medium': require('../../assets/fonts/Poppins-Medium.otf'),
     });
+
+    this.setState({
+      fontLoaded: true,
+    });
   }
 
   render() {
     return (
-      <View>
-        <Text>NotificationScreen</Text>
-        <Text>NotificationScreen</Text>
-        <Text>NotificationScreen</Text>
-        <Text>NotificationScreen</Text>
-        <Text>NotificationScreen</Text>
-        <Text>NotificationScreen</Text>
+      <View style={styles.Back}>
+        {this.state.fontLoaded ? (
+          <View style={styles.Back}>
+            <Header
+              centerComponent={{ text: 'Notifications', style: styles.HeaderText }}
+              backgroundColor="#FFFFFF"
+            />
+          </View>
+        ) : null}
       </View>
     );
   }
---
+++ src/styles/NotificationScreenStyle.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  Back: {
+    flex: 1,
+    backgroundColor: '#F5F5F5',
+  },
+  HeaderText: {
+    marginTop: 12,
+    fontFamily: 'poppins-regular',
+    color: '#000000',
+    fontSize: 22,
+  },
+});
+
+export default styles;
