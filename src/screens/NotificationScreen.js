c0332e214df23aee273e4ffc43db617337e2aa38
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-08-23 20:09:40 +0530

    NotificationScreen.js: Add sample content

--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -8,6 +8,78 @@ import styles from '../styles/NotificationScreenStyle';
 class NotificationScreen extends Component {
   state = {
     fontLoaded: false,
+    notifications: [
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '1',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '2',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '3',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '4',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '5',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '6',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '7',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '8',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '9',
+      },
+      {
+        profile_pic: require('../../assets/icons/ph-pic.png'),
+        date: '11 March',
+        time: '6:24 PM',
+        content: 'Pizza House added 100 points in your Account',
+        key: '10',
+      },
+    ],
   };
 
   async componentDidMount() {
