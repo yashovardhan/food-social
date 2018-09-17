12bdff25e101905bcd176607d0e0753a4cb0870f
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-09-18 03:27:59 +0530

    SearchScreen: Designing part complete

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,26 +1,122 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image, TextInput, FlatList } from 'react-native';
 import { Font } from 'expo';
+import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+
+import icoMoonConfig from '../../assets/selection.json';
+import styles from '../styles/SearchScreenStyle';
+
+const Iconset = createIconSetFromIcoMoon(icoMoonConfig, 'icomoon');
 
 class SearchScreen extends Component {
+  state = {
+    fontLoaded: false,
+    restaurants: [
+      {
+        name: 'Asian Darbar',
+        address: 'Four square building malviya nagar',
+        cuisine: 'Rajasthani, Asian, North indian',
+        profile: require('../../assets/images/search-profile.jpg'),
+        key: '1',
+      },
+      {
+        name: 'Asian Darbar',
+        address: 'Four square building malviya nagar',
+        cuisine: 'Rajasthani, Asian, North indian',
+        profile: require('../../assets/images/search-profile.jpg'),
+        key: '2',
+      },
+      {
+        name: 'Asian Darbar',
+        address: 'Four square building malviya nagar',
+        cuisine: 'Rajasthani, Asian, North indian',
+        profile: require('../../assets/images/search-profile.jpg'),
+        key: '3',
+      },
+      {
+        name: 'Asian Darbar',
+        address: 'Four square building malviya nagar',
+        cuisine: 'Rajasthani, Asian, North indian',
+        profile: require('../../assets/images/search-profile.jpg'),
+        key: '4',
+      },
+    ],
+    background: require('../../assets/images/search-background.jpg'),
+  };
+
   async componentDidMount() {
     await Font.loadAsync({
       'poppins-bold': require('../../assets/fonts/Poppins-Bold.otf'),
       'poppins-light': require('../../assets/fonts/Poppins-Light.otf'),
       'poppins-regular': require('../../assets/fonts/Poppins-Regular.otf'),
       'poppins-medium': require('../../assets/fonts/Poppins-Medium.otf'),
+      icomoon: require('../../assets/fonts/icomoon.ttf'),
     });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  unorderedList(data) {
+    return (
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.OfferDetails}>{'\u2022'}</Text>
+        <Text style={[styles.OfferDetails, { flex: 1, paddingLeft: 5 }]}>{data}</Text>
+      </View>
+    );
   }
 
   render() {
     return (
-      <View>
-        <Text>SearchScreen</Text>
-        <Text>SearchScreen</Text>
-        <Text>SearchScreen</Text>
-        <Text>SearchScreen</Text>
-        <Text>SearchScreen</Text>
-        <Text>SearchScreen</Text>
+      <View style={styles.Back}>
+        {this.state.fontLoaded ? (
+          <View style={styles.Back}>
+            <View style={styles.GraphicArea}>
+              <Image source={this.state.background} resizeMode="cover" style={styles.Graphic} />
+              <Text style={styles.FormHeading}>Search for restaurants and add them here</Text>
+              <View style={styles.Form}>
+                <Iconset name="search" size={15} color="#AFAFAF" />
+                <TextInput
+                  style={[styles.FormInput, { marginLeft: 14 }]}
+                  onChangeText={text => this.setState({ email: text })}
+                  value={this.state.email}
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  clearButtonMode="while-editing"
+                  placeholder="Search Restaurant"
+                  underlineColorAndroid="rgba(0,0,0,0)"
+                />
+              </View>
+            </View>
+            <View style={styles.DetailsArea}>
+              <FlatList
+                data={this.state.restaurants}
+                style={styles.FlatList}
+                renderItem={({ item }) => (
+                  <View style={styles.Card}>
+                    <View style={styles.CardContent}>
+                      <Image
+                        source={item.profile}
+                        resizeMode="contain"
+                        style={styles.CardProfile}
+                      />
+                      <View style={styles.CardHeadings}>
+                        <Text style={styles.RestaurantName}>{item.name}</Text>
+                        <Text style={styles.RestaurantDetails}>{item.address}</Text>
+                        <Text style={styles.RestaurantDetails}>Cuisines - {item.cuisine}</Text>
+                      </View>
+                    </View>
+                    <TouchableOpacity
+                      style={styles.CardButton}
+                      onPress={() => this.props.navigation.navigate('details')}>
+                      <Text style={styles.CardButtonText}>BOOK A TABLE</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
+                keyExtractor={item => item.key}
+              />
+            </View>
+          </View>
+        ) : null}
       </View>
     );
   }
---
+++ src/styles/SearchScreenStyle.js
@@ -0,0 +1,100 @@
+import { StyleSheet, Platform } from 'react-native';
+
+const styles = StyleSheet.create({
+  Back: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  GraphicArea: {
+    flex: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Graphic: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  DetailsArea: {
+    flex: 12,
+    backgroundColor: '#F5F5F5',
+    padding: 12,
+  },
+  FormHeading: {
+    fontFamily: 'poppins-medium',
+    fontSize: 13,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    margin: 10,
+  },
+  Form: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 40,
+    marginRight: 40,
+    padding: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderColor: '#FFFFFF',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 5,
+  },
+  FormIcon: {
+    height: 20,
+    width: 20,
+  },
+  FormInput: {
+    flex: 10,
+    marginTop: Platform.OS === 'android' ? 5 : 0,
+    marginLeft: 15,
+    height: 20,
+    fontFamily: 'poppins-regular',
+    fontSize: 15,
+  },
+  Card: {
+    height: 125,
+    margin: 10,
+    borderRadius: 5,
+    overflow: 'hidden',
+  },
+  CardContent: {
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    padding: 10,
+  },
+  CardProfile: {
+    height: 65,
+    position: 'absolute',
+    margin: 10,
+    marginLeft: -5,
+  },
+  CardButton: {
+    flex: 1,
+    backgroundColor: '#FF5F35',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  CardButtonText: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontFamily: 'poppins-light',
+    fontSize: 15,
+    color: '#FFFFFF',
+  },
+  CardHeadings: {
+    marginLeft: 80,
+  },
+  RestaurantName: {
+    fontFamily: 'poppins-medium',
+    fontSize: 17,
+  },
+  RestaurantDetails: {
+    fontFamily: 'poppins-light',
+    fontSize: 11,
+    color: '#AFAFAF',
+  },
+});
+
+export default styles;
